0afa881c6e583ed5eaae65abed98203a8ea3572f
Zeynal <z.abasov@turkmen-tranzit.com>
2021-03-04 17:32:49 +0500

    Added Employee Configuratins

---
+++ DSEU.Domain/Entities/CoreEntities/AuthenticationType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSEU.Domain.Entities.CoreEntities
+{
+    /// <summary>
+    /// Тип аутентификации
+    /// </summary>
+    public enum AuthenticationType
+    {
+        Password
+    }
+}
---
+++ DSEU.Infrastructure.Persistence/Configurations/OurOrganization/JobTitleConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using DSEU.Domain.Entities.OurOrganization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DSEU.Infrastructure.Persistence.Configurations.OurOrganization
+{
+    public class JobTitleConfiguration : IEntityTypeConfiguration<JobTitle>
+    {
+        public void Configure(EntityTypeBuilder<JobTitle> builder)
+        {
+            builder.Property(p => p.Name).IsRequired();
+            builder.HasIndex(p => p.Name).IsUnique();
+        }
+    }
+}
